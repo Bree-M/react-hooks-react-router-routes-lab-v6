56b93fe7ecc050e1b46360e84fc3058ad0771e39
Bree-M <brenda.wairimu@student.moringaschool.com>
2025-04-29 00:59:08 +0300

    make my changes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,9 +3,13 @@ import "./NavBar.css";
 
 function NavBar() {
   return (
-    <nav className="navbar">
-    
-    </nav>
+    <div className="navbar" role="navigation">
+    <NavLink to="/" end>
+      Home
+    </NavLink>
+    <NavLink to="/actors">Actors</NavLink>
+    <NavLink to="/directors">Directors</NavLink>
+  </div>
     );
 };
 
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,7 +1,27 @@
-
+import Home from "./pages/Home";
+import Actors from "./pages/Actors";
+import Directors from "./pages/Directors";
+import Movie from "./pages/Movie";
+import Error from "./pages/Error";
 
 const routes = [
-   
-  ];
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <Error />
+  },
+  {
+    path: "/actors",
+    element: <Actors />
+  },
+  {
+    path: "/directors",
+    element: <Directors />
+  },
+  {
+    path: "/movie/:id",
+    element: <Movie />
+  }
+];
 
 export default routes;
